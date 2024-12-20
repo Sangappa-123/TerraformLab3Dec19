a1a34df8123585164634a4f0da72949f8d711fab
Sangappa-123 <birajsd@gmail.com>
2024-12-20 13:36:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraformstate-1234"
-    key            = "state/terraform.tfstate"
+    bucket         = "my-terraform-state-bucket-1212"
+    key            = "oidc/terraform.tfstate"
     region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
   }
 }
